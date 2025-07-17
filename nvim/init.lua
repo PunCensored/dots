0ddda7c4ef207505d89a3ee9b8b1ce5e4ada2158
PunCensored <jemandlebt@gmail.com>
2025-07-17 21:40:22 +0200

    update nvim lsp config

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,6 +4,12 @@ require('lualine').setup()
 vim.wo.number = true
 -- vim.wo.relativenumber = true
 
+-- Set nvim.notify as default notification
+vim.notify = require("notify")
 
 -- Open Neotree on 'n'
 vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
+
+vim.lsp.enable('pyrefly')
+
+vim.cmd [[autocmd! CursorHold * lua vim.diagnostic.config({ virtual_lines = { only_current_line = true } })]]
