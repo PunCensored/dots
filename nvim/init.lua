6a58073a7c039851e07edb219e0956c3bf73b50d
PunCensored <jemandlebt@gmail.com>
2025-11-05 22:27:56 +0100

    typst preparations
    typst preperations

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,13 +12,15 @@ vim.keymap.set('n', '<leader>a',':quit<CR>')
 vim.keymap.set('n', '<leader>w',':write<CR>')
 vim.keymap.set({'n', 'v', 'x'}, '<leader>y','"+y<CR>')
 vim.keymap.set({'n', 'v', 'x'}, '<leader>d','"+d<CR>')
+vim.keymap.set('n', '<leader>vs',':vsplit<CR>')
 -- Plugin Keymaps
 vim.keymap.set('n', '<leader>nn',':Neotree<CR>')
 vim.keymap.set('n', '<leader>nd',':Neotree close<CR>')
+vim.keymap.set('n', '<leader>p',':TypstPreview<CR>')
 
 require("config.lazy")
 require('lualine').setup()
 
-vim.lsp.enable({'luals', 'pyright'})
+vim.lsp.enable({'luals', 'pyright', 'tinymist'})
 vim.lsp.config("luals",
 { settings = {Lua = {workspace = { library = vim.api.nvim_get_runtime_file("", true)}}}})
