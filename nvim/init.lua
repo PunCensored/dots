fc68475893ca038a50253fae75ea1cf4a9613ad2
PunCensored <jemandlebt@gmail.com>
2025-11-11 20:27:51 +0100

    add window ctl keymaps to nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,13 +6,24 @@ vim.opt.winborder = "none"
 vim.o.swapfile = false
 vim.o.wildmenu = true
 vim.o.wildoptions = "pum"
+
 -- Vanilla Keymaps
 vim.g.mapleader = " "
 vim.keymap.set('n', '<leader>a',':quit<CR>')
 vim.keymap.set('n', '<leader>w',':write<CR>')
 vim.keymap.set({'n', 'v', 'x'}, '<leader>y','"+y<CR>')
 vim.keymap.set({'n', 'v', 'x'}, '<leader>d','"+d<CR>')
+
 vim.keymap.set('n', '<leader>vs',':vsplit<CR>')
+vim.keymap.set('n', '<leader>hs',':split<CR>')
+
+vim.keymap.set('n', '<leader>b',':bprevious<CR>')
+
+vim.keymap.set('n', '<leader>rn',':wincmd h')
+vim.keymap.set('n', '<leader>re',':wincmd j')
+vim.keymap.set('n', '<leader>ri',':wincmd k')
+vim.keymap.set('n', '<leader>ro',':wincmd l')
+vim.keymap.set('n', '<leader>rp',':wincmd p')
 -- Plugin Keymaps
 vim.keymap.set('n', '<leader>nn',':Neotree<CR>')
 vim.keymap.set('n', '<leader>nd',':Neotree close<CR>')
