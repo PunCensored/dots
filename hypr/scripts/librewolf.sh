c8278bbc0cf5243550dc6742ff9c0362670645b0
PunCensored <jemandlebt@gmail.com>
2026-08-10 21:19:25 +0200

    okay maybe 8 am is a bit late for non-browsing.

--- hypr/scripts/librewolf.sh
+++ hypr/scripts/librewolf.sh
@@ -2,7 +2,7 @@
 hour="$(date +%H)"
 hour=$((10#$hour))
 echo $hour
-if [[ $hour -ge 22 ]] || [[ $hour -le 7 ]]; then
+if [[ $hour -ge 22 ]] || [[ $hour -le 4 ]]; then
 	kitty -e bash -c 'echo "Too late. Go to sleep."; sleep 1'
 else
 	/usr/bin/librewolf &
